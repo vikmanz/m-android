4d7b5b8c879f856780429627370648e22bdf6329
VikManz <vikmanza@gmail.com>
2023-01-23 19:23:23 +0200

    first commit

---
+++ app/src/main/java/com/vikmanz/shpppro/MyProfile.kt
@@ -0,0 +1,13 @@
+package com.vikmanz.shpppro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_profile)
+        supportActionBar?.hide()
+
+    }
+}
---
+++ app/src/main/java/com/vikmanz/shpppro/SignUp.kt
@@ -0,0 +1,12 @@
+package com.vikmanz.shpppro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+        supportActionBar?.hide()
+    }
+}
