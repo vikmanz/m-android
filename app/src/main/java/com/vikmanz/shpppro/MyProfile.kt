8c1b687b400db19dc9dc4c1d57424e16b3b4f3fd
VikManz <vikmanza@gmail.com>
2023-01-23 20:45:15 +0200

    up

--- app/src/main/java/com/vikmanz/shpppro/MyProfile.kt
+++ app/src/main/java/com/vikmanz/shpppro/MyProfile.kt
@@ -7,7 +7,5 @@ class MyProfile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_profile)
-        supportActionBar?.hide()
-
     }
 }
