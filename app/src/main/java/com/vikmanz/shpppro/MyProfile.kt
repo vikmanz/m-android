f7a379edd9705c856a42fb0d9cbb4f486175691b
VikManz <vikmanza@gmail.com>
2023-01-28 21:08:04 +0200

    2023-01-23 #4

--- app/src/main/java/com/vikmanz/shpppro/MyProfile.kt
+++ app/src/main/java/com/vikmanz/shpppro/MyProfile.kt
@@ -2,10 +2,22 @@ package com.vikmanz.shpppro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
 
 class MyProfile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_profile)
+
+        val button: AppCompatButton = findViewById(R.id.btnViewMyContacts)
+
+
+
+
+        button.setOnClickListener {
+            finish()
+        }
+
     }
+
 }
--- app/src/main/java/com/vikmanz/shpppro/SignUp.kt
+++ app/src/main/java/com/vikmanz/shpppro/SignUp.kt
@@ -1,12 +1,28 @@
 package com.vikmanz.shpppro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
 
 class SignUp : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_profile)
+        setContentView(R.layout.activity_sign_up)
+
+        val button: AppCompatButton = findViewById(R.id.bRegisterByEmailPassword)
+
+
+
+
+        button.setOnClickListener {
+            val changePage = Intent(this, MyProfile::class.java)
+            startActivity(changePage)
+        }
 
     }
+
+
+
 }
