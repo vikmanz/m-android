8f3ca43212825861e4eff7bc60088114df23cfbd
VikManz <vikmanza@gmail.com>
2023-02-15 22:21:22 +0200

    2023-02-15 end

--- app/src/main/java/com/vikmanz/shpppro/MainActivity.kt
+++ app/src/main/java/com/vikmanz/shpppro/MainActivity.kt
@@ -52,9 +52,6 @@ class MainActivity : AppCompatActivity() {
         // Set onClick listener to Logout button.
         binding.tvLogout.setOnClickListener { logout() }
 
-        // Set onClick listener to MyContacts button.
-        binding.btnViewMyContacts.setOnClickListener { startMyContactsActivity() }
-
     }
 
     /**
@@ -145,17 +142,6 @@ class MainActivity : AppCompatActivity() {
         super.onRestoreInstanceState(savedInstanceState)
         isUkrainian = savedInstanceState.getBoolean(LANGUAGE_STATE_KEY_TWO)
         setLocale()
-    }
-
-    /**
-     * Start My contacts activity.
-     */
-    private fun startMyContactsActivity() {
-        val intentObject = Intent(this, MyContactsActivity::class.java)
-        intentObject.putExtra(INTENT_LANG_ID, isUkrainian)
-        startActivity(intentObject)
-        overridePendingTransition(R.anim.zoom_in_inner, R.anim.zoom_in_outter)
-        finish()
     }
 
 }
