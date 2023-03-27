3dbd607d1a6efea290dc86e3ee7e44057e7d272e
Vik <vikmanza@gmail.com>
2023-03-27 19:48:16 +0300

    sql

--- app/src/main/java/com/vikmanz/shpppro/data/contactModel/ContactsPhoneInfoTaker.kt
+++ app/src/main/java/com/vikmanz/shpppro/data/contactModel/ContactsPhoneInfoTaker.kt
@@ -20,43 +20,99 @@ class ContactsPhoneInfoTaker() {
         val contentResolver = App.instance.contentResolver
 
         val uri = ContactsContract.Data.CONTENT_URI
-        val projection = arrayOf(
-            ContactsContract.Data.CONTACT_ID,
-            ContactsContract.Contacts.DISPLAY_NAME,
-            ContactsContract.CommonDataKinds.Phone.NUMBER
+        val projection =
+            arrayOf(
+            ContactsContract.CommonDataKinds.Phone.CONTACT_ID,
+            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+            ContactsContract.CommonDataKinds.Phone.NUMBER,
+            ContactsContract.CommonDataKinds.Organization.COMPANY
         )
-        val selection =
-            ContactsContract.Data.MIMETYPE + "='" + ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE + "'"
-        val sortOrder = ContactsContract.Contacts.DISPLAY_NAME
+
+        val selection = null
+//            ContactsContract.Data.MIMETYPE + " in (?, ?) AND " +
+//                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?"
+
+        val selectionArgs =
+            arrayOf(
+            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+            ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE,
+            ContactsContract.CommonDataKinds.Organization.CONTENT_ITEM_TYPE
+        )
+        val sortOrder = null
+
+        log("""
+              _FROM [table_name]: $uri
+              _projection [col,col,col,...]: ${arrayToString(projection)}
+              _WHERE [WHERE col = value]: $selection
+              _selectionArgs : ${arrayToString(selectionArgs)}
+              _ORDER BY [ORDER BY col,col,...]: $sortOrder
+           _""".trimMargin("_"))
 
         val cursor = contentResolver.query(uri, projection, selection, null, sortOrder)
+        log("<<<--- cursor created --->>>")
+        if (cursor != null) {
 
-        cursor?.let {
-            Log.d("myLog", "Total contacts count: ${cursor.count}")
+            log("count of columns ${cursor.columnCount}")
+            log("columns: ${arrayToString(cursor.columnNames)}")
+            log("count of lines ${cursor.count}")
+
+            //cursor.close()
+            log("--->>> cursor closed <<<---")
+
             while (cursor.moveToNext()) {
 
-                val id = cursor.getLong(cursor.getColumnIndex(ContactsContract.Data.CONTACT_ID))
+                val column0 =
+                    cursor.getString(0)
+                val column1 =
+                    cursor.getString(1)
+                val column2 =
+                    cursor.getString(3)
+                val column3 =
+                    cursor.getString(3)
 
-                val name: String = cursor.getString(
-                    cursor.getColumnIndex(
-                        ContactsContract.Contacts.DISPLAY_NAME
-                    )
-                )
-
-                val phone: String = cursor.getString(
-                    cursor.getColumnIndex(
-                        ContactsContract.CommonDataKinds.Phone.NUMBER
-                    )
-                )
-
-                val company: String = getContactCompanyName(contentResolver, id)
-
-                log("Adding ::: id: $id, name: $name, phone: $phone, company: $company")
-                listOfContactsInformation.add(listOf(name, phone))
+                log("column0: $column0, column1: $column1, column2: $column2, column3: $column3")
 
             }
             cursor.close()
         }
+//        val uri = ContactsContract.Data.CONTENT_URI
+//        val projection = arrayOf(
+//            ContactsContract.Data.CONTACT_ID,
+//            ContactsContract.Contacts.DISPLAY_NAME,
+//            ContactsContract.CommonDataKinds.Phone.NUMBER
+//        )
+//        val selection =
+//            ContactsContract.Data.MIMETYPE + "='" + ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE + "'"
+//        val sortOrder = ContactsContract.Contacts.DISPLAY_NAME
+//
+//        val cursor = contentResolver.query(uri, projection, selection, null, sortOrder)
+//
+//        cursor?.let {
+//            Log.d("myLog", "Total contacts count: ${cursor.count}")
+//            while (cursor.moveToNext()) {
+//
+//                val id = cursor.getLong(cursor.getColumnIndex(ContactsContract.Data.CONTACT_ID))
+//
+//                val name: String = cursor.getString(
+//                    cursor.getColumnIndex(
+//                        ContactsContract.Contacts.DISPLAY_NAME
+//                    )
+//                )
+//
+//                val phone: String = cursor.getString(
+//                    cursor.getColumnIndex(
+//                        ContactsContract.CommonDataKinds.Phone.NUMBER
+//                    )
+//                )
+//
+//                val company: String = getContactCompanyName(contentResolver, id)
+//
+//                log("Adding ::: id: $id, name: $name, phone: $phone, company: $company")
+//                listOfContactsInformation.add(listOf(name, phone))
+//
+//            }
+//            cursor.close()
+//        }
         return listOfContactsInformation
     }
 
@@ -79,4 +135,10 @@ class ContactsPhoneInfoTaker() {
         return "[no have a company]"
     }
 
+    private fun arrayToString(array: Array<String>): String {
+        var result = "array of ["
+        array.forEach { result += "$it, " }
+        result = result.substring(0, result.length - 2) + "]"
+        return result
+    }
 }
